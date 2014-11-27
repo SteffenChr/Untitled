23be3494429185571d1444089752477994a233ff
Hilindi <kwhjaltason@hotmail.com>
2014-11-27 20:29:22 +0100

    Added PlayerHealt
    Added lines of code in PlayerHealt

--- Assets/Scripts/Player/PlayerHealt.cs
+++ Assets/Scripts/Player/PlayerHealt.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class PlayerHealt : MonoBehaviour
 {
-	
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.transform.CompareTag("Enemy"))
+        {
+            print("Hit by enemy");
+        }
+    }
 }
 
