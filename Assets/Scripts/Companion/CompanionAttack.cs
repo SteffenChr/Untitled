bad07d88a2ec1666674a4f02ee3fc0759a216dd5
Hilindi <kwhjaltason@hotmail.com>
2014-12-15 20:04:44 +0100

    New scripts
    Created a new script struckture for the attacks, now they use
    inheritance

---
+++ Assets/Scripts/Companion/CompanionAttack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompanionAttack : MonoBehaviour {
+
+    public int damageDealing = 0;
+    public Transform transform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
